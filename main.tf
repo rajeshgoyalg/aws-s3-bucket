20e384f38354f3f6ac990477e9138457e54431c5
Rajesh Goyal <rajesh.goyal@qoala.id>
2025-04-16 23:09:13 +0530

    Your commit message here

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "aws_s3_bucket" "this" {
-    bucket = var.bucket_name
-    tags   = var.tags
+  bucket = var.bucket_name
+  tags   = var.tags
 }
 
 resource "aws_s3_bucket_versioning" "this" {
-    bucket = aws_s3_bucket.this.id
-    versioning_configuration {
-        status = var.versioning
-    }
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = var.versioning
+  }
 
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "bucket_name" {
-    description = "The name of the bucket. If omitted, Terraform will assign a random, unique name."
-    type        = string
+  description = "The name of the bucket. If omitted, Terraform will assign a random, unique name."
+  type        = string
 }
 
 variable "tags" {
-    description = "A map of tags to assign to the bucket."
-    type        = map(string)
+  description = "A map of tags to assign to the bucket."
+  type        = map(string)
 }
 
 variable "versioning" {
-    description = "Enable versioning on the bucket."
-    type        = string
-    default     = "Enabled" # or "Suspended"
+  description = "Enable versioning on the bucket."
+  type        = string
+  default     = "Enabled" # or "Suspended"
 }
